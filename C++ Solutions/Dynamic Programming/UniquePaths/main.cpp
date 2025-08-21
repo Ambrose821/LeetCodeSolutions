3fb4132d860394e740af6df7ab85e28f41485528
Ambrose821 <mclaughlinambrose@gmail.com>
2025-08-21 16:56:13 -0700

    Added C++ Shortest Path

---
+++ C++ Solutions/Dynamic Programming/UniquePaths/main.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> paths(m, vector<int>(n));
+        for(int i =0; i< m; i ++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0){
+                    paths[i][j] = 1;
+                    continue;
+                }
+                if(i==0 && j !=0){
+                    paths[i][j] =1;
+                    continue;
+                }
+                if(j==0 && i!=0){
+                    paths[i][j]=1;
+                    continue;
+                }
+                paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                
+
+            }   
+        }
+        return paths[m-1][n-1];
+    }
+};
